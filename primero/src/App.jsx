1b8e3093491a454e7fc99c1c711aa85352ee591d
Alexandra Pinto <alexandra_pinto2015hotmail.com>
2025-07-08 00:57:00 -0400

    Primer commit general del proyecto

--- primero/src/App.jsx
+++ primero/src/App.jsx
@@ -5,22 +5,39 @@ const API_KEY = '219e6fdf368a2ca4a7764ecf73c0ef24';
 function App() {
   const [texto, setTexto] = useState('');
   const [error, setError] = useState('');
+  const [message, setMessage] = useState('');
 
   const validar = () => {
+
     if (texto === '') return;
     fetch(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=es-ES&query=${texto}`)
       .then(response => response.json())
       .then(data => {
         if (data.results.length === 0) {
-          setError('No se encontro.');
-        } else {
           setError('');
+          setMessage('No se encontraron los datos.');
+        } else {
+          setMessage('Datos cargados');
+          setError('');
+          console.log("Datos cargados: ", data.results);
         }
       })
       .catch(() => {
-        setError('Error al buscar');
+        setError('Error al cargar');
+        setMessage('');
       });
   };
+
+  return (
+    <div>
+      <input type="text" value={texto} onChange={(e) => setTexto(e.target.value)} placeholder="Buscar una película"/>
+
+      <button onClick={validar}>Buscar pelicula</button>
+
+      {message && <p>{message}</p>}
+      {error && <p>{error}</p>}
+    </div>
+  );
 }
 
 export default App;
